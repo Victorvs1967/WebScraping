7bfadbc96c4d701db44cf3e93e31a1db53960d78
Victorvs1967 <victorsmirnov@me.com>
2019-06-07 15:27:33 +0300

    Initial Commit

---
+++ WebScraping/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  WebScraping
+//
+//  Created by Victor Smirnov on 07/06/2019.
+//  Copyright © 2019 Victor Smirnov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
